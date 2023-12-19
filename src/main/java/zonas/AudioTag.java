8d4b6ce5a554c8c7cc34aaa7d7bedeb26c24fb6f
MLChinoo <40102068+MLChinoo@users.noreply.github.com>
2023-12-19 12:06:13 +0800

    Auto convert artwork PNG to JPG

--- src/main/java/zonas/AudioTag.java
+++ src/main/java/zonas/AudioTag.java
@@ -5,8 +5,15 @@ import org.jaudiotagger.audio.AudioFileIO;
 import org.jaudiotagger.tag.FieldKey;
 import org.jaudiotagger.tag.Tag;
 import org.jaudiotagger.tag.datatype.Artwork;
+import org.jaudiotagger.tag.id3.valuepair.ImageFormats;
+import org.jaudiotagger.tag.reference.PictureTypes;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
@@ -39,10 +46,23 @@ public class AudioTag {
                 }
             });
 
-            Artwork artwork = new Artwork();
-            artwork.setMimeType("image/jpeg");
-            artwork.setBinaryData(Util.fileDownload(picurl));
-            tag.setField(artwork);
+            byte[] picData = Util.fileDownload(picurl);
+            String mimeType = ImageFormats.getMimeTypeForBinarySignature(picData);
+            if (!mimeType.equals("image/jpeg")) {
+                if (mimeType.equals("image/png")) {
+                    picData = convertPNGtoJPG(picData);
+                } else {
+                    System.out.println("Unsupported mimeType: " + mimeType);
+                    picData = null;
+                }
+            }
+            if (picData != null) {
+                Artwork artwork = new Artwork();
+                artwork.setBinaryData(picData);
+                artwork.setMimeType(ImageFormats.getMimeTypeForBinarySignature(artwork.getBinaryData()));
+                artwork.setPictureType(PictureTypes.DEFAULT_ID);
+                tag.setField(artwork);
+            }
 
             audioFile.setTag(tag);
             audioFile.commit();
@@ -51,6 +71,25 @@ public class AudioTag {
         } catch (Exception e) {
             e.printStackTrace();
             return false;
+        }
+    }
+
+    private static byte[] convertPNGtoJPG(byte[] pngBytes) {
+        try {
+            ByteArrayInputStream input = new ByteArrayInputStream(pngBytes);
+            BufferedImage originalImage = ImageIO.read(input);
+
+            BufferedImage newImage = new BufferedImage(originalImage.getWidth(), originalImage.getHeight(), BufferedImage.TYPE_INT_RGB);
+            newImage.createGraphics().drawImage(originalImage, 0, 0, null);
+            newImage.createGraphics().dispose();
+
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+            ImageIO.write(newImage, "jpg", output);
+
+            return output.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
